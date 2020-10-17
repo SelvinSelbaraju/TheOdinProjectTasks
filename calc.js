01b2b3373dac0f62ab0d41b63d64cfb856292a79
SelvinSelbaraju <selvinoselbaraju@gmail.com>
2020-10-17 16:36:30 +0100

    workingCalc

--- calc.js
+++ calc.js
@@ -3,8 +3,10 @@ let display = "";
 let numberButtons = document.querySelectorAll('.num-btn');
 let signButtons = document.querySelectorAll('.sign-btn');
 let signs = [' + ',' - ',' * ',' / '];
-let clearButton = document.querySelector('#clear');
+let clearButton = document.querySelector('#clear-all');
+let cButton = document.querySelector('#clear');
 let equalButton = document.querySelector('#equals');
+let decimalPoint = document.querySelector('#dot');
 let stopScreen = false;
 let showSigns = false; 
 
@@ -30,7 +32,7 @@ function operate(operator,num1,num2) {
 } 
 
 function displayNums(num) {
-    if (stopScreen == false) {
+    if (display.length < 30 && stopScreen == false) {
         display = display + num;
         displayDiv.textContent = display;
         showSigns = true;
@@ -38,7 +40,7 @@ function displayNums(num) {
 }
 
 function displaySigns(sign) {
-    if (showSigns == true) {
+    if (display != "" && display.length < 30 && showSigns == true && display.split(" ").length < 2) {
         display = display + sign;
         displayDiv.textContent = display;
         stopScreen = false;
@@ -48,6 +50,23 @@ function displaySigns(sign) {
     
 }
 
+function singleClear() {
+    if (display.length > 0) {
+        if (display == 'Infinity') {
+            display = "";
+        }
+
+        else if (display.charAt(display.length-1) == " ") {
+        display = display.slice(0, -3);
+        }
+        else {
+            display = display.slice(0, -1);
+        }
+    }
+    displayDiv.textContent = display;
+    stopScreen = false; 
+}
+
 function clearNums() {
     display = "";
     displayDiv.textContent = display;
@@ -55,29 +74,37 @@ function clearNums() {
     showSigns = false;
 }
 
-function calculateResult () {
-    num1 = Number(display.split(" ")[0]);
-    num2 = Number(display.split(" ")[2]);
-    sign = display.split(" ")[1];
-
-    if (sign == '+') {
-        display = num1 + num2;
-    }
-
-    if (sign == '-') {
-        display = num1 - num2;
-    }
-
-    if (sign == '*') {
-        display = num1 * num2;
-    }
-
-    if (sign == '/') {
-        display = num1 / num2;
-    }
-    display = String(display);
+function addDP() {
+    display = display + ".";
     displayDiv.textContent = display;
-    stopScreen = true;
+}
+
+
+function calculateResult () {
+    if (display.split(" ").length > 2) {
+        num1 = Number(display.split(" ")[0]);
+        num2 = Number(display.split(" ")[2]);
+        sign = display.split(" ")[1];
+
+        if (sign == '+') {
+            display = num1 + num2;
+        }
+
+        if (sign == '-') {
+            display = num1 - num2;
+        }
+
+        if (sign == '*') {
+            display = num1 * num2;
+        }
+
+        if (sign == '/') {
+            display = num1 / num2;
+        }
+        display = String(display);
+        displayDiv.textContent = display;
+        stopScreen = true;
+    }
 }
 
 for (let i=0; i < numberButtons.length; i++) {
@@ -92,6 +119,8 @@ clearButton.addEventListener('click',clearNums);
 
 equalButton.addEventListener('click',calculateResult);
 
+decimalPoint.addEventListener('click',addDP);
 
+cButton.addEventListener('click',singleClear)
 
 
